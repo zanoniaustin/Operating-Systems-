f27bfcb9d5507130f7bb2123aae5ed1c18a6e42f
zanoniaustin <austinzanoni21@gmail.com>
2018-01-30 13:21:45 -0800

    homework 1&2

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Operating-SystemS-
-hello
+
+## Homework 1 hello.c - running hello.c on virtual machine
+
+## Homework 2 runNano.c - takes the runls.c code and opens up a nano .txt
---
+++ runNano.c
@@ -0,0 +1,26 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int main(int argc, char *argv[])
+{
+	printf("'shell' is running\n");
+	pid_t child_pid;
+	child_pid = fork();
+	if (child_pid == 0)
+	{
+  		printf("child started\n");
+  		execl("/bin/nano", "nano", "testRunNano.txt", NULL);
+  		printf("child ended\n");
+  		return 3;
+  	}
+	else
+	{
+   		printf("parent waiting for child\n");
+   		int child_status;
+   		waitpid(child_pid, &child_status, 0);
+   		printf("child excited with status %d\n", (child_status/256));
+	}
+ 	return 0;
+}
